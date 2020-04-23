8a1dce33e4b2818308bc496ff354e8b74150d0cd
SS-CN-JIANG.XIOAYU <>
2020-04-23 16:15:02 +0800

    second commit

--- Sorting/Sort/3InsertionSort.go
+++ Sorting/Sort/3InsertionSort.go
@@ -15,7 +15,12 @@ func InsertionSort(arr []int) {
 
 func InsertionSortT(arr []int) {
 	l := len(arr)
-	for i := 1; i < len(arr); i++ {
-
+	for i := 0; i < l-1; i++ {
+		preIndex := i + 1
+		current := arr[preIndex]
+		for ; preIndex > 0 && arr[preIndex-1] < current; preIndex-- {
+			arr[preIndex] = arr[preIndex-1]
+		}
+		arr[preIndex] = current
 	}
 }
--- Sorting/main.go
+++ Sorting/main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	arr := []int{99, 23, 21, 6, 7, 54, 2, 84, 51, 12, 8, 71, 48, 1, 29}
 	fmt.Println(len(arr))
-	Sort.InsertionSort(arr)
+	Sort.InsertionSortT(arr)
 	fmt.Println(arr)
 }
