65815606d709646c6f50869f3864c3079ea06de3
SS-CN-JIANG.XIOAYU <>
2020-06-09 22:13:39 +0800

    add 0610

--- labuladong/Chapter1/3_test.go
+++ labuladong/Chapter1/3_test.go
@@ -1,1 +1,31 @@
 package Chapter1_test
+
+import (
+	"testing"
+)
+
+func BB(wt, val []int, N, W int) int {
+	dp := make([][]int, N+1)
+	for index := range dp {
+		dp[index] = make([]int, W+1)
+	}
+	for i := 1; i <= N; i++ {
+		for w := 1; w <= W; w++ {
+			//println("N:", strconv.Itoa(i), " W:", strconv.Itoa(w))
+			if w-wt[i-1] > 0 {
+				dp[i][w] = max(dp[i-1][w], dp[i-1][w-wt[i-1]]+val[i-1])
+			} else {
+				dp[i][w] = dp[i-1][w]
+			}
+		}
+	}
+	return dp[N][W]
+}
+
+func Test_BB(t *testing.T) {
+	N, W := 3, 4
+	wt := []int{2, 1, 3}
+	val := []int{4, 2, 3}
+	data := BB(wt, val, N, W)
+	println(data)
+}
