2ef974d5a4e2fc4cf7a01df533693f6c25b93465
蒋晓宇 <jiang.xiaoyu@eland.co.kr>
2021-04-22 15:59:22 +0800

    200422

--- LeetCode_Test/Array/middle_test/91_test.go
+++ LeetCode_Test/Array/middle_test/91_test.go
@@ -1,5 +1,10 @@
 package middle_test
 
+import (
+	"github.com/jxy90/GoTest/LeetCode_Test/BinaryTree/Study/Codec"
+	"math"
+)
+
 var ans int = 0
 
 func numDecodings(s string) int {
@@ -15,4 +20,31 @@ func numDecodings(s string) int {
 		}
 	}
 	return dp[n]
+}
+
+func maxPathSum(root *Codec.TreeNode) int {
+	min := math.MinInt32
+	var maxPathSumHelper func(node *Codec.TreeNode) int
+	maxPathSumHelper = func(root *Codec.TreeNode) int {
+		if root == nil {
+			return 0
+		}
+
+		leftMax := max(maxPathSumHelper(root.Left), 0)
+		rightMax := max(maxPathSumHelper(root.Right), 0)
+
+		lrmax := max(leftMax, rightMax)
+
+		min = max(root.Val+leftMax+rightMax, min)
+
+		return root.Val + lrmax
+	}
+	return min
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
---
+++ LeetCode_Test/String/hard_test/44_test.go
@@ -0,0 +1,85 @@
+package hard_test
+
+import (
+	"strings"
+	"testing"
+)
+
+func isMatch(s string, p string) bool {
+	slen := len(s)
+	plen := len(p)
+	dp := make([][]bool, slen+1)
+	for i := range dp {
+		dp[i] = make([]bool, plen+1)
+	}
+	for i := range p {
+		if p[i] == '*' {
+			dp[0][i+1] = true
+		} else {
+			break
+		}
+	}
+	dp[0][0] = true
+	for i := 0; i < slen; i++ {
+		for j := 0; j < plen; j++ {
+			if s[i] == p[j] || p[j] == '?' {
+				dp[i+1][j+1] = dp[i][j]
+			} else if p[j] == '*' {
+				dp[i+1][j+1] = dp[i+1][j] || dp[i][j+1]
+			} else {
+				dp[i+1][j+1] = false
+			}
+		}
+	}
+	return dp[slen][plen]
+}
+func isMatch1(s string, p string) bool {
+	for strings.Contains(p, "**") {
+		p = strings.ReplaceAll(p, "**", "*")
+	}
+	return isMatchHelper(s, p, 0, 0)
+}
+
+func isMatchHelper(s string, p string, i, j int) bool {
+	//两个指针到指向终点 匹配
+	if j == len(p) && i == len(s) {
+		return true
+	}
+	//p剩余* 匹配
+	if p[j:] == "*" {
+		return true
+	}
+	//s指向终点 p还有剩余
+	if i == len(s) {
+		return false
+	}
+	// s和p剩余长度不相等，且p不包含*
+	if !strings.Contains(p[j:], "*") && len(s[i:]) != len(p[j:]) {
+		return false
+	}
+	if p[j] == s[i] {
+		//相等，对比下一位
+		return isMatchHelper(s, p, i+1, j+1)
+	} else if p[j] == '?' {
+		//p==？，对比下一位
+		return isMatchHelper(s, p, i+1, j+1)
+	} else if p[j] == '*' {
+		//p==*。对剩余长度进行全匹配，党有一种情况满足，返回true
+		flag := false
+		for k := 0; len(s)-i-k > 0; k++ {
+			flag = isMatchHelper(s, p, i+k, j+1)
+			if flag {
+				return flag
+			}
+		}
+	}
+	return false
+}
+
+func Test_isMath(t *testing.T) {
+	println(isMatch("a", ""))
+	println(isMatch("aaaa", "***a"))
+	println(isMatch("adceb", "****a*b"))
+	println(isMatch("", "******"))
+	println(isMatch("leetcode", "*e*t?d*"))
+}
