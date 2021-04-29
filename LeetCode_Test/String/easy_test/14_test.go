824cfd4dfb294b833673e55afcbd7256755c81a4
蒋晓宇 <jiang.xiaoyu@eland.co.kr>
2021-04-29 09:22:36 +0800

    14. 最长公共前缀 添加注释

--- LeetCode_Test/String/easy_test/14_test.go
+++ LeetCode_Test/String/easy_test/14_test.go
@@ -8,8 +8,12 @@ func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
 	}
+	//把第一个string作为模式串
 	for i := 0; i < len(strs[0]); i++ {
+		//从1开始，后面的string，作为比较串
 		for j := 1; j < len(strs); j++ {
+			// 当模式串的长度>比较串的时候，判断相等。返回的是 比较串的本身
+			// 当模式串和比较串相同位置不相等时，返回的是 开始位置到当前位置
 			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
 				return strs[0][:i]
 			}
