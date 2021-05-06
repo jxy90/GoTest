0d5d40fe0234e1d16c95d25414c75acce21510ad
jxy <279089367@qq.com>
2021-05-06 17:00:33 +0800

    781. 森林中的兔子

---
+++ LeetCode_Test/DP/middle_test/781_test.go
@@ -0,0 +1,30 @@
+package middle_test
+
+import (
+	"testing"
+)
+
+func Test_numRabbits(t *testing.T) {
+	println(numRabbits([]int{1, 0, 1, 0, 0}))
+	println(numRabbits([]int{1, 1, 2}))
+}
+
+//4ms 2.8MB
+func numRabbits(answers []int) int {
+	//保存爆出自己颜色的兔子有多少只
+	hash := make(map[int]int, 0)
+	for _, v := range answers {
+		//加一的作用是加上自己的数量
+		hash[v+1]++
+	}
+	ans := 0
+	for key := range hash {
+		if key == 0 {
+			ans += hash[key]
+		} else {
+			group := (hash[key]-1)/key + 1
+			ans += key * group
+		}
+	}
+	return ans
+}
