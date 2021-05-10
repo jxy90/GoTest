ae8850306e470b74184ce7d82a18158f2cb6d3b7
jxy <279089367@qq.com>
2021-05-10 11:12:07 +0800

    872. 叶子相似的树

--- LeetCode_Test/BinaryTree/easy_test/872_test.go
+++ LeetCode_Test/BinaryTree/easy_test/872_test.go
@@ -5,9 +5,54 @@ import (
 	"testing"
 )
 
-var str1, str2 string
+func Test_leafSimilar(t *testing.T) {
+	root1 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+	}
+	root2 := &TreeNode{
+		Val: 2,
+		Left: &TreeNode{
+			Val: 2,
+		},
+	}
+	data := leafSimilar(root1, root2)
+	println(data)
+}
 
 func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	nums1 := make([]int, 0)
+	nums2 := make([]int, 0)
+	leafSimilarInOrder(root1, &nums1)
+	leafSimilarInOrder(root2, &nums2)
+	if len(nums1) != len(nums2) {
+		return false
+	}
+	for i := 0; i < len(nums1); i++ {
+		if nums1[i] != nums2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func leafSimilarInOrder(node *TreeNode, nums *[]int) {
+	if node == nil {
+		return
+	}
+	leafSimilarInOrder(node.Left, nums)
+	if node.Left == nil && node.Right == nil {
+		*nums = append(*nums, node.Val)
+		return
+	}
+	leafSimilarInOrder(node.Right, nums)
+}
+
+var str1, str2 string
+
+func leafSimilar0(root1 *TreeNode, root2 *TreeNode) bool {
 	str1, str2 = "", ""
 	leafSimilarDFS(root1, "1")
 	leafSimilarDFS(root2, "2")
@@ -27,21 +72,4 @@ func leafSimilarDFS(root *TreeNode, str string) {
 		}
 	}
 	leafSimilarDFS(root.Right, str)
-}
-
-func Test_leafSimilar(t *testing.T) {
-	root1 := &TreeNode{
-		Val: 1,
-		Left: &TreeNode{
-			Val: 2,
-		},
-	}
-	root2 := &TreeNode{
-		Val: 2,
-		Left: &TreeNode{
-			Val: 2,
-		},
-	}
-	data := leafSimilar(root1, root2)
-	println(data)
 }
