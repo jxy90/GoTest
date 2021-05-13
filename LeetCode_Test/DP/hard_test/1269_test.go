854e190657ce5d95ab589ce62634cd2b3baed4eb
jxy <279089367@qq.com>
2021-05-13 09:44:35 +0800

    1269. 停在原地的方案数

---
+++ LeetCode_Test/DP/hard_test/1269_test.go
@@ -0,0 +1,58 @@
+package DP_test
+
+import (
+	"github.com/jxy90/GoTest/Utils/CommonUtil"
+	"testing"
+)
+
+func Test_numWays(t *testing.T) {
+	println(numWays(3, 2))
+	println(numWays(2, 4))
+	println(numWays(4, 2))
+	println(numWays(47, 38))
+}
+
+var mod = 1000000000 + 7
+
+func numWays(steps int, arrLen int) int {
+	arrLen = CommonUtil.Min(steps, arrLen)
+	f := make([]int, arrLen+1)
+
+	options := []int{-1, 0, 1}
+	//f[i][j] 表示通过i步到j位置的方案数目
+	f[0] = 1
+	for i := 0; i < steps; i++ {
+		nextf := make([]int, arrLen+1)
+		for j := 0; j < arrLen; j++ {
+			for k := 0; k < 3; k++ {
+				nextJ := j + options[k]
+				if nextJ >= 0 && nextJ < arrLen {
+					nextf[nextJ] = (nextf[nextJ] + f[j]) % mod
+				}
+			}
+		}
+		f = nextf
+	}
+	return f[0]
+}
+
+func numWays0(steps int, arrLen int) int {
+	f := make([][]int, steps+1)
+	for i := range f {
+		f[i] = make([]int, arrLen+1)
+	}
+	options := []int{-1, 0, 1}
+	//f[i][j] 表示通过i步到j位置的方案数目
+	f[0][0] = 1
+	for i := 0; i < steps; i++ {
+		for j := 0; j < arrLen; j++ {
+			for k := 0; k < 3; k++ {
+				nextJ := j + options[k]
+				if nextJ >= 0 && nextJ < arrLen {
+					f[i+1][nextJ] = (f[i+1][nextJ] + f[i][j]) % mod
+				}
+			}
+		}
+	}
+	return f[steps][0]
+}
