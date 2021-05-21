ea02ab9b5fffec0208947db3296be62c211faf46
jxy <279089367@qq.com>
2021-05-21 19:12:37 +0800

    1036

---
+++ LeetCode_Test/Array/hard_test/1035_test.go
@@ -0,0 +1,31 @@
+package hard_test
+
+import (
+	"github.com/jxy90/GoTest/Utils/CommonUtil"
+	"testing"
+)
+
+func Test_maxUncrossedLines(t *testing.T) {
+	//println(maxUncrossedLines([]int{1, 4, 2}, []int{1, 2, 4}))
+	//println(maxUncrossedLines([]int{2, 5, 1, 2, 5}, []int{10, 5, 2, 1, 5, 2}))
+	println(maxUncrossedLines([]int{1, 3, 7, 1, 7, 5}, []int{1, 9, 2, 5, 1}))
+}
+
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	//f[i][j] 第i,j个位置连线的方案数
+	m := len(nums1)
+	n := len(nums2)
+	f := make([][]int, m+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			f[i][j] = CommonUtil.Max(f[i-1][j], f[i][j-1])
+			if nums1[i] == nums2[j] {
+				f[i][j] = CommonUtil.Max(f[i-1][j-1]+1, f[i][j])
+			}
+		}
+	}
+	return f[m][n]
+}
