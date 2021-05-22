7bc3fd3794f3d3c7b55ca530e117d05689b82566
jxy <279089367@qq.com>
2021-05-22 22:54:10 +0800

    810. 黑板异或游戏

---
+++ LeetCode_Test/Array/hard_test/810_test.go
@@ -0,0 +1,15 @@
+package hard_test
+
+import "testing"
+
+func Test_xorGame(t *testing.T) {
+	println(xorGame([]int{1, 1, 2}))
+}
+
+func xorGame(nums []int) bool {
+	sum := 0
+	for i := range nums {
+		sum ^= nums[i]
+	}
+	return sum == 0 || len(nums)%2 == 0
+}
