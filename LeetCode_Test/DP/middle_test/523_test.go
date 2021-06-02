3726dd59e9e1cd4f7d4dae6e476b85f82649827e
jxy <279089367@qq.com>
2021-06-02 10:36:35 +0800

    523. 连续的子数组和

---
+++ LeetCode_Test/DP/middle_test/523_test.go
@@ -0,0 +1,48 @@
+package middle_test
+
+import "testing"
+
+func Test_checkSubarraySum(t *testing.T) {
+	println(checkSubarraySum([]int{23, 2, 4, 6, 6}, 7))
+	println(checkSubarraySum([]int{1, 0}, 2))
+	println(checkSubarraySum([]int{5, 0, 0, 0}, 3))
+	println(checkSubarraySum([]int{23, 2, 6, 4, 7}, 13))
+	println(checkSubarraySum([]int{23, 2, 4, 6, 7}, 6))
+}
+
+func checkSubarraySum(nums []int, k int) bool {
+	n := len(nums)
+	f := make(map[int]int)
+	f[0] = -1
+	rem := 0
+	for i := 0; i < n; i++ {
+		rem += nums[i]
+		if index, ok := f[rem%k]; ok {
+			if i-index > 1 {
+				return true
+			}
+		} else {
+			f[rem%k] = i
+		}
+	}
+	return false
+}
+func checkSubarraySum0(nums []int, k int) bool {
+	n := len(nums)
+	f := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		f[i] = f[i-1] + nums[i-1]
+	}
+	m := len(f)
+	for i := 0; i < m; i++ {
+		for j := i + 1; j < m; j++ {
+			if i == 0 && j == m-1 {
+				continue
+			}
+			if (f[j]-f[i])%k == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
