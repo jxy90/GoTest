3f318772b1fa637f19c6a857b6d292c8d9178f74
jiangxiaoyu <279089367@qq.com>
2021-07-06 17:18:26 +0800

    backPack backPackII

---
+++ LeetCode_Test/Backpack/backPackII_test.go
@@ -0,0 +1,47 @@
+package Backpack
+
+import (
+	"github.com/jxy90/GoTest/Utils/CommonUtil"
+	"testing"
+)
+
+/*
+有 n 个物品和一个大小为 m 的背包. 给定数组 A 表示每个物品的大小和数组 V 表示每个物品的价值.
+
+问最多能装入背包的总价值是多大?
+*/
+
+//物品1个,01背包
+func Test_backPackII(t *testing.T) {
+	println(backPackII(10, []int{2, 3, 5, 7}, []int{1, 5, 2, 4}))
+}
+
+func backPackII(m int, A []int, V []int) int {
+	n := len(A)
+	f := make([]int, m+1)
+	for i := 0; i < n; i++ {
+		for j := m; j >= A[i]; j-- {
+			f[j] = CommonUtil.Max(f[j], f[j-A[i]]+V[i])
+		}
+	}
+	return f[m]
+}
+
+func backPackII0(m int, A []int, V []int) int {
+	n := len(A)
+	//前i个物品在大小为j的背包下,最大量
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j <= m; j++ {
+			if j-A[i] >= 0 {
+				dp[i+1][j] = CommonUtil.Max(dp[i][j], dp[i][j-A[i]]+V[i])
+			} else {
+				dp[i+1][j] = dp[i][j]
+			}
+		}
+	}
+	return dp[n][m]
+}
---
+++ LeetCode_Test/Backpack/backPack_test.go
@@ -0,0 +1,45 @@
+package Backpack
+
+import (
+	"github.com/jxy90/GoTest/Utils/CommonUtil"
+	"testing"
+)
+
+/*
+在n个物品中挑选若干物品装入背包，最多能装多满？假设背包的大小为m，每个物品的大小为A{i}
+*/
+
+//物品一个,01背包
+func Test_backPack(t *testing.T) {
+	println(backPack(10, []int{3, 4, 8, 5}))
+}
+
+func backPack(m int, A []int) int {
+	n := len(A)
+	f := make([]int, m+1)
+	for i := 0; i < n; i++ {
+		for j := m; j >= A[i]; j-- {
+			f[j] = CommonUtil.Max(f[j], f[j-A[i]]+A[i])
+		}
+	}
+	return f[m]
+}
+
+func backPack0(m int, A []int) int {
+	n := len(A)
+	//前i个物品在大小为j的背包下,最大量
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j <= m; j++ {
+			if j-A[i] >= 0 {
+				dp[i+1][j] = CommonUtil.Max(dp[i][j], dp[i][j-A[i]]+A[i])
+			} else {
+				dp[i+1][j] = dp[i][j]
+			}
+		}
+	}
+	return dp[n][m]
+}
