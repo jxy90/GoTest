ed69cbbce49b8c4432dddf2f354abc69fc4d713b
jiangxiaoyu <279089367@qq.com>
2021-07-20 18:07:30 +0800

    33. 搜索旋转排序数组

---
+++ LeetCode_Test/BinarySearch/middle_test/33_test.go
@@ -0,0 +1,37 @@
+package easy_test
+
+import (
+	"testing"
+)
+
+func Test_search(t *testing.T) {
+	println(search([]int{3, 1}, 1))
+	println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0))
+}
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)>>1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] >= nums[0] {
+			//左边有序
+			if nums[0] <= target && target <= nums[mid] {
+				//搜索左边
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else {
+			//右边有序
+			if nums[mid] <= target && target <= nums[len(nums)-1] {
+				//搜索右边
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return -1
+}
