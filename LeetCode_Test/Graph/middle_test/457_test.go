dc35d7708267b8c3941f5a21f43b0f1273394357
jiangxiaoyu <279089367@qq.com>
2021-08-11 09:51:27 +0800

    457. 环形数组是否存在循环

--- LeetCode_Test/Graph/middle_test/457_test.go
+++ LeetCode_Test/Graph/middle_test/457_test.go
@@ -1,10 +1,40 @@
 package middle_test_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_circularArrayLoop(t *testing.T) {
 	println(circularArrayLoop([]int{2, -1, 1, 2, 2}))
 }
 func circularArrayLoop(nums []int) bool {
+	n := len(nums)
+	check := func(start int) bool {
+		cur := start
+		flag := nums[start] > 0
+		k := 1
+		for {
+			if k > n {
+				return false
+			}
+			next := ((cur+nums[cur])%n + n) % n
+			if flag && nums[next] < 0 {
+				return false
+			}
+			if !flag && nums[next] > 0 {
+				return false
+			}
+			if next == start {
+				return k > 1
+			}
+			cur = next
+			k++
+		}
+	}
+	for i := range nums {
+		if check(i) {
+			return true
+		}
+	}
 	return false
 }
