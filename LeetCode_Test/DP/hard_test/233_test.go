323289823ca240153a7df6a184b130c9c2b42734
jiangxiaoyu <279089367@qq.com>
2021-08-13 10:24:38 +0800

    233. 数字 1 的个数

---
+++ LeetCode_Test/DP/hard_test/233_test.go
@@ -0,0 +1,48 @@
+package DP_test
+
+import (
+	"github.com/jxy90/GoTest/Utils/CommonUtil"
+	"testing"
+)
+
+func Test_countDigitOne(t *testing.T) {
+	println(countDigitOne(13))
+}
+
+//计算每一位1出现的次数
+func countDigitOne(n int) int {
+	//i表示n的位数,每次进位
+	//从个位开始
+	//1234567为例,计算十位出现1的次数
+	count := 0
+	for i := 1; i <= n; i *= 10 {
+		//十位的1被循环的次数
+		//12345
+		a := n / (i * 10)
+		//十位
+		//67
+		b := n % (i * 10)
+		//十位大于1234500,出现1的次数
+		//10
+		c := CommonUtil.Min(CommonUtil.Max(b-i+1, 0), i)
+		//12345 + 10
+		count += a*i + c
+	}
+	return count
+}
+
+func countDigitOne0(n int) int {
+	f := make([]int, n+1)
+	f[0] = 0
+	ans := 0
+	for i := 1; i <= n; i++ {
+		j := i / 10
+		count := 0
+		if i%10 == 1 {
+			count = 1
+		}
+		f[i] = f[j] + count
+		ans += f[i]
+	}
+	return ans
+}
---
+++ LeetCode_Test/DP/middle_test/516_test.go
@@ -0,0 +1,34 @@
+package middle_test
+
+import (
+	"github.com/jxy90/GoTest/Utils/CommonUtil"
+	"testing"
+)
+
+func Test_longestPalindromeSubseq(t *testing.T) {
+	println(longestPalindromeSubseq("bbbab"))
+}
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+	//f[i][j]表示i~j范围内回文的长度
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+		for j := range f[i] {
+			if i == j {
+				f[i][j] = 1
+			}
+		}
+	}
+	for length := 1; length <= n; length++ {
+		for i := 0; i < n-length; i++ {
+			j := i + length
+			if s[i] == s[j] {
+				f[i][j] = f[i+1][j-1] + 2
+			} else {
+				f[i][j] = CommonUtil.Max(f[i+1][j], f[i][j-1])
+			}
+		}
+	}
+	return f[0][n-1]
+}
