6f753eba0fc3fc04569e51e50db9bf14e9caabf4
jiangxiaoyu <279089367@qq.com>
2021-08-25 11:30:52 +0800

    797. 所有可能的路径

---
+++ LeetCode_Test/BFSDFS/middle_test/797_test.go
@@ -0,0 +1,30 @@
+package middle_test
+
+import (
+	"testing"
+)
+
+func Test_allPathsSourceTarget(t *testing.T) {
+	println(allPathsSourceTarget([][]int{{1, 2}, {3}, {3}, {}}))
+}
+
+func allPathsSourceTarget(graph [][]int) [][]int {
+	ans := make([][]int, 0)
+	n := len(graph)
+	var dfs func([]int, int)
+	dfs = func(ints []int, index int) {
+		if index == n-1 {
+			temp := make([]int, len(ints))
+			copy(temp, ints)
+			ans = append(ans, temp)
+			return
+		}
+		for _, v := range graph[index] {
+			ints = append(ints, v)
+			dfs(ints, v)
+			ints = ints[:len(ints)-1]
+		}
+	}
+	dfs([]int{0}, 0)
+	return ans
+}
