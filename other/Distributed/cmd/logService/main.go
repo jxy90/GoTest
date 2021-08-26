dc8acd868be664bb0b43c9e108e914b113697a8c
jiangxiaoyu <279089367@qq.com>
2021-08-26 14:58:06 +0800

    Distributed

--- other/Distributed/cmd/logService/main.go
+++ other/Distributed/cmd/logService/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/jxy90/GoTest/other/distributed/log"
-	"github.com/jxy90/GoTest/other/distributed/service"
+	"github.com/jxy90/GoTest/other/Distributed/log"
+	"github.com/jxy90/GoTest/other/Distributed/service"
 	stlog "log"
 )
 
 func main() {
-	log.Run("./distributed.log")
+	log.Run("./Distributed.log")
 	host, port := "localhost", "4000"
 	ctx, err := service.Start(context.Background(), "Log Service", host, port, log.RegisterHandlers)
 	if err != nil {
