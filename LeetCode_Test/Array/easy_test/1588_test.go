9aa44f9f2bda27e9b6783c90645fb62fe88aa1e3
jiangxiaoyu <279089367@qq.com>
2021-08-29 12:45:44 +0800

    1588. 所有奇数长度子数组的和

---
+++ LeetCode_Test/Array/easy_test/1588_test.go
@@ -0,0 +1,23 @@
+package easy_test
+
+import (
+	"testing"
+)
+
+func Test_sumOddLengthSubarrays(t *testing.T) {
+	println(sumOddLengthSubarrays([]int{1, 4, 2, 5, 3}))
+}
+func sumOddLengthSubarrays(arr []int) int {
+	n := len(arr)
+	sum := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		sum[i+1] = sum[i] + arr[i]
+	}
+	ans := 0
+	for length := 1; length <= n; length += 2 {
+		for i := 0; i+length <= n; i++ {
+			ans += sum[i+length] - sum[i]
+		}
+	}
+	return ans
+}
