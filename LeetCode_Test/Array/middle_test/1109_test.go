48466d248e3da1d7fccdb4a534e0e0c08e8f8cbe
jiangxiaoyu <279089367@qq.com>
2021-08-31 10:21:09 +0800

    1109. 航班预订统计

---
+++ LeetCode_Test/Array/middle_test/1109_test.go
@@ -0,0 +1,20 @@
+package middle_test
+
+import "testing"
+
+func Test_corpFlightBookings(t *testing.T) {
+	println(corpFlightBookings([][]int{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}}, 5))
+}
+
+//差分
+func corpFlightBookings(bookings [][]int, n int) []int {
+	nums := make([]int, n+2)
+	for _, booking := range bookings {
+		nums[booking[0]] += booking[2]
+		nums[booking[1]+1] -= booking[2]
+	}
+	for i := 1; i < n+1; i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums[1 : len(nums)-1]
+}
