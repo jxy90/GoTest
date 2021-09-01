e983db02c8ed993ad5998450791fd9a264f1c480
jiangxiaoyu <279089367@qq.com>
2021-09-01 20:56:04 +0800

    go5

---
+++ LeetCode_Test/String/middle_test/165_test.go
@@ -0,0 +1,32 @@
+package middle_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func Test_compareVersion(t *testing.T) {
+	println(compareVersion("1.01", "1.001"))
+}
+
+func compareVersion(version1, version2 string) int {
+	v1 := strings.Split(version1, ".")
+	v2 := strings.Split(version2, ".")
+	for i := 0; i < len(v1) || i < len(v2); i++ {
+		x, y := 0, 0
+		if i < len(v1) {
+			x, _ = strconv.Atoi(v1[i])
+		}
+		if i < len(v2) {
+			y, _ = strconv.Atoi(v2[i])
+		}
+		if x > y {
+			return 1
+		}
+		if x < y {
+			return -1
+		}
+	}
+	return 0
+}
