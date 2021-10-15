e35d2314636af5c309ee9c6ab8883ce575e02cd3
jiangxiaoyu <279089367@qq.com>
2021-10-15 17:10:19 +0800

    go_part7_test.go

--- other/Fast/go_part7_test.go
+++ other/Fast/go_part7_test.go
@@ -2,6 +2,8 @@ package Fast
 
 import (
 	"fmt"
+	"image"
+	"log"
 	"math/rand"
 	"strings"
 	"sync"
@@ -140,4 +142,69 @@ func (v *Visited) VisitedLink(url string) int {
 	count++
 	v.visited[url] = count
 	return count
+}
+
+func Test31_2(t *testing.T) {
+	r := NewRoverDriver()
+	time.Sleep(3 * time.Second)
+	r.Left()
+	time.Sleep(3 * time.Second)
+	r.Right()
+	time.Sleep(3 * time.Second)
+	r.Left()
+}
+
+type command int
+
+const (
+	right = command(0)
+	left  = command(1)
+)
+
+type RoverDriver struct {
+	commandc chan command
+}
+
+func NewRoverDriver() *RoverDriver {
+	r := &RoverDriver{
+		commandc: make(chan command),
+	}
+	go r.drive()
+	return r
+}
+
+func (r *RoverDriver) drive() {
+	pos := image.Point{X: 0, Y: 0}
+	direction := image.Point{X: 1, Y: 0}
+	updateInternal := 250 * time.Millisecond
+	nextMove := time.After(updateInternal)
+	for {
+		select {
+		case c := <-r.commandc:
+			switch c {
+			case right:
+				direction = image.Point{
+					X: -direction.Y,
+					Y: direction.X,
+				}
+			case left:
+				direction = image.Point{
+					X: direction.Y,
+					Y: -direction.X,
+				}
+			}
+			log.Printf("new direction %v", direction)
+		case <-nextMove:
+			pos = pos.Add(direction)
+			log.Printf("moved to %v", pos)
+			nextMove = time.After(updateInternal)
+		}
+	}
+}
+
+func (r *RoverDriver) Left() {
+	r.commandc <- left
+}
+func (r *RoverDriver) Right() {
+	r.commandc <- right
 }
