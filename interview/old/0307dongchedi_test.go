a4af13b782ba5df3a1022c34b94001313d751997
jiangxiaoyu <1990jxy@gmail.com>
2022-03-07 16:39:53 +0800

    懂车帝

--- interview/old/0307dongchedi_test.go
+++ interview/old/0307dongchedi_test.go
@@ -6,25 +6,25 @@ import (
 	"testing"
 )
 
-func Test_0(t *testing.T) {
+func Test_dongchedi(t *testing.T) {
 	//fmt.Println("123")
 	c1 := make(chan int)
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 1; i <= 100; i++ {
 			c1 <- 1
-			if i%2 == 0 {
-				fmt.Print(i)
+			if i%2 == 1 {
+				fmt.Println(i)
 			}
 		}
 		wg.Done()
 	}()
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 1; i <= 100; i++ {
 			<-c1
-			if i%2 == 1 {
-				fmt.Print(i)
+			if i%2 == 0 {
+				fmt.Println(i)
 			}
 		}
 		wg.Done()
