e61d7bb64cb9bb4f18d68eb71f787da19c3d6095
jiangxiaoyu <1990jxy@gmail.com>
2022-03-16 17:04:48 +0800

    go1.18泛型支持

--- Test/T/Demo.go
+++ Test/T/Demo.go
@@ -9,9 +9,16 @@ func Print[T any](s []T) {
 	for _, v := range s {
 		fmt.Print(v)
 	}
+	fmt.Println()
+}
+
+type People struct {
+	name string
+	age  int
 }
 
 func main() {
 	Print([]string{"你好, ", "脑子进了煎鱼\n"})
 	Print([]int64{1, 2, 3})
+	Print([]People{{"1", 1}, {"2", 2}})
 }
