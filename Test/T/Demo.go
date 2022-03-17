f21bfadb9b3103b6ac6f4c8c924dc436a72275c1
jiangxiaoyu <1990jxy@gmail.com>
2022-03-17 09:21:40 +0800

    泛型

--- Test/T/Demo.go
+++ Test/T/Demo.go
@@ -17,6 +17,10 @@ type People struct {
 	age  int
 }
 
+func (p People) String() string {
+	return fmt.Sprintf("{name:%v age: %v} ", p.name, p.age)
+}
+
 func main() {
 	Print([]string{"你好, ", "脑子进了煎鱼\n"})
 	Print([]int64{1, 2, 3})
