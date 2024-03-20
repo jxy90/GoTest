d1e8f2f21e83560b0dfce7bfe99e36c6cc86c17e
jiangxiaoyu <1990jxy@gmail.com>
2024-03-20 12:28:03 +0800

    Robotaxi

--- Test/main.go
+++ Test/main.go
@@ -3,20 +3,137 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
-	once := &sync.Once{}
-	//wg := &sync.WaitGroup{}
+	fmt.Println(findKthLargest([]int{8, 3456, 8, 34, 76, 383, 25, 6, 23, 45, 7, 8, 56, 34, 3}, 1))
 
-	for i := 0; i < 4; i++ {
-		i := i
-		go func() {
-			once.Do(func() {
-				fmt.Printf("first %d\n", i)
-			})
-		}()
+	//mapval := Map{
+	//	Cache:  make(map[string]*entry),
+	//	Locker: &sync.RWMutex{},
+	//}
+	//
+	//for i := 0; i < 10; i++ {
+	//	go func(i int) {
+	//		val := mapval.Read(string(i), time.Second*6)
+	//		log.Println("读取值为->", val)
+	//	}(i)
+	//}
+	//
+	//time.Sleep(time.Second * 3)
+	//for i := 0; i < 10; i++ {
+	//	go func(val int) {
+	//		mapval.Write(string(val), val)
+	//	}(i)
+	//}
+	//
+	//time.Sleep(time.Second * 30)
+
+	//print10()
+	//printLetter()
+}
+
+func findKthLargest(nums []int, k int) []int {
+	helper(nums, k)
+	return nums
+}
+
+func helper(nums []int, k int) {
+	if len(nums) < 2 {
+		return
 	}
-	time.Sleep(time.Second)
+	left, right := 0, len(nums)-1
+	mid, i := nums[left], 1
+	for left < right {
+		if nums[i] > mid {
+			nums[i], nums[left] = nums[left], nums[i]
+			i++
+			left++
+		} else {
+			nums[i], nums[right] = nums[right], nums[i]
+			right--
+		}
+	}
+	helper(nums[:left], k)
+	helper(nums[left+1:], k-left-1)
+}
+
+func printLetter() {
+	fmt.Println()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	letter, number := make(chan struct{}), make(chan struct{})
+
+	go func() {
+		i := 1
+		for {
+			select {
+			case <-number:
+				print(i)
+				i++
+				print(i)
+				i++
+				letter <- struct{}{}
+			}
+		}
+	}()
+	go func() {
+		i := 'A'
+		for {
+			select {
+			case <-letter:
+				if i > 'Z' {
+					wg.Done()
+					break
+				}
+				print(string(i))
+				i++
+				print(string(i))
+				i++
+				number <- struct{}{}
+			}
+		}
+	}()
+
+	number <- struct{}{}
+	wg.Wait()
+}
+
+func print10() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	defer func() {
+		close(ch1)
+		close(ch2)
+		ch1 = nil
+		ch2 = nil
+	}()
+	go func() {
+		for {
+			select {
+			case val := <-ch1:
+				if val > 10 {
+					wg.Done()
+					break
+				}
+				print(val)
+				ch2 <- val + 1
+			}
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case val := <-ch2:
+
+				print(val)
+				ch1 <- val + 1
+			}
+		}
+	}()
+
+	ch1 <- 1
+	wg.Wait()
 }
---
+++ interview/old/2024/0320didi1_test.go
@@ -0,0 +1,121 @@
+package _024
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+)
+
+// 要求：
+// 1. 只能编辑 foo 函数
+// 2. foo 必须要调用 slow 函数
+// 3. foo 函数在 ctx 超时后必须立刻返回
+// 4. 【加分项】如果 slow 结束的比 ctx 快，也立刻返回
+
+func Test_024(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	foo(ctx)
+}
+
+// 您需要实现 foo 函数，要求 foo 在 ctx 超时后立即返回
+// foo 必须调用 slow 函数
+func foo(ctx context.Context) {
+	ch := make(chan struct{}, 0)
+	go func() {
+		slow()
+		ch <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		ch <- struct{}{}
+		return
+	case <-ch:
+		return
+	}
+}
+
+// 您不能修改 slow 函数
+func slow() {
+	n := rand.Intn(3)
+	fmt.Printf("sleep %ds\n", n)
+	time.Sleep(time.Duration(n) * time.Second)
+}
+
+// 我们有一张学生的分数表 scores:
+// name, subject, score
+// 张三 语文   81
+// 张三 数学   75
+// 李四 语文   85
+// 李四 数学   90
+
+// 要求：用一条 SQL 查出每门课都大于 80 分的学生的姓名
+
+//select name form scores group by name having min(score)>80;
+
+// 给你两个版本号 `version1` 和 `version2` ，请你比较它们。
+// 版本号由多个部分组成，每个部分由一个 '.' 连接。每个部分都由数字组成，可能包含前导零。
+// 返回规则如下：
+// 如果 version1 > version2 返回 1，
+// 如果 version1 < version2 返回 -1，
+// 除此之外返回 0
+
+// 示例：
+// 1.0 > 0.1
+// 1.1 > 1.0
+// 1.01 == 1.1
+// 1.0.0 == 1.0
+
+func compareVersion(v1, v2 string) int {
+	num1 := versionToNums(v1)
+	num2 := versionToNums(v2)
+	//双指针比较
+	p1, p2 := 0, 0
+	for p1 < len(num1) && p2 < len(num2) {
+		val1, val2 := num1[p1], num2[p2]
+		if val1 > val2 {
+			return 1
+		} else if val1 < val2 {
+			return -1
+		}
+		p1++
+		p2++
+	}
+
+	//剩余元素判断
+	if p1 != len(num1) {
+		for p1 < len(num1) {
+			if num1[p1] != 0 {
+				return 1
+			}
+			p1++
+		}
+	}
+	if p2 != len(num2) {
+		for p2 < len(num2) {
+			if num2[p2] != 0 {
+				return -1
+			}
+			p2++
+		}
+	}
+	return 0
+}
+
+func versionToNums(version string) []int {
+	strs := strings.Split(version, ".")
+	nums := make([]int, len(strs))
+	for i, str := range strs {
+		for _, b := range str {
+			nums[i] = nums[i]*10 + int(b-'0')
+		}
+	}
+	return nums
+}
