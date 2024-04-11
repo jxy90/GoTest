4ae8684995be7402fcc6a787f9b9088b8eee56ee
jiangxiaoyu <1990jxy@gmail.com>
2024-04-11 16:43:21 +0800

    kuaishou2

--- interview/old/2024/0411kuaishou2_test.go
+++ interview/old/2024/0411kuaishou2_test.go
@@ -12,52 +12,33 @@ func Test_JO(t *testing.T) {
 	fmt.Println(nums)
 }
 
-//1 7 3 9 5 2 6 4 8
-// func helper(nums []int){
-//     cnt := 0
-//     for _,v :=range nums{
-//         if !isX(v) {
-//             cnt++
-//         }
-//     }
-//     p1,p2,p3:=0,0,cnt
-//     for p1<len(nums) && p2 <len(nums){
-//         for !isX(nums[p1]){
-//             p1++
-//         }
-//         for isX(nums[p2]){
-//             p2++
-//         }
-//     }
-
-//     for p1<cnt && p2<len(nums) {
-//         for !isX(nums[p1]){
-//             p1++
-//         }
-//         if p1 < cnt {
-//             nums[p1],nums[p2]=nums[p2],nums[p1]
-//             p2++
-//         }
-//     }
-// }
-
-//奇偶混杂的数组，最后返回奇数在前
 func helper(nums []int) {
-	left, right := 0, len(nums)-1
-	for left < right {
-		for !isX(nums[left]) {
-			left++
-		}
-		for isX(nums[right]) {
-			right--
-		}
-		if left < right {
-			nums[left], nums[right] = nums[right], nums[left]
-			left++
-			right--
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			if isX(nums[i]) && !isX(nums[j]) {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
 		}
 	}
 }
+
+//奇偶混杂的数组，最后返回奇数在前
+//func helper(nums []int) {
+//	left, right := 0, len(nums)-1
+//	for left < right {
+//		for !isX(nums[left]) {
+//			left++
+//		}
+//		for isX(nums[right]) {
+//			right--
+//		}
+//		if left < right {
+//			nums[left], nums[right] = nums[right], nums[left]
+//			left++
+//			right--
+//		}
+//	}
+//}
 
 func isX(num int) bool {
 	return num%2 == 0
